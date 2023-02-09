ea88c4452a37ded70db18d3de16e052b6bfe7cdf
Santhosh <santhoshkalish333@gmail.com>
2023-02-10 01:52:50 +0530

    fully working

--- mastermind.py
+++ mastermind.py
@@ -3,46 +3,60 @@
 import random
 import re
 
-def main():
-    colors = ['R', 'G', 'B', 'Y', 'W', 'O']
-    guess = 3
+COLORS = ['R', 'G', 'B', 'Y', 'W', 'O']
+max_tries = 10
 
+def main():
+    
     print(""" 
         WELCOME TO MASTERMIND  
         
         Attempt to guess the 4 digit code...
         you have 10 tries.
         """)
-    print(f'The colors that could make up the code are: R G B Y W O')
+    print(f'The colors that could make up the code are: R G B Y W O\n')
 
-    code = []
-    for _ in range(4):
-        code.append(random.choice(colors))
-
+    code = [random.choice(COLORS) for _ in range(4)]
+    tries = 0
+    
     print(code)
 
-    while guess > 0:
-        crt_guess = 0
-        incrt_guess = 0
+    while tries < max_tries:
+        crct_guess = 0
+        incrct_guess = 0
         
         user_guess = input('Guess (space seperated): ')
-        if validate_guess(user_guess):
-            print('valid')
+        guess_list = validate_guess(user_guess)
+        if guess_list:
+            for i in range(4):
+                if guess_list[i] == code[i]:
+                    crct_guess += 1
+                else:
+                    incrct_guess += 1
         else:
             print('Invalid Guess')
             
-        guess -= 1
+        tries += 1
         
+        if crct_guess == 4:
+            print(f'You guessed the code in {tries} tries!')
+            break
+        else:
+            print(f'Correct position: {crct_guess} | Incorrect possion: {incrct_guess}\n')
+            
+            
 def validate_guess(guess):
     guess = guess.strip().upper()
-    if re.match(r'^[A-Z] [A-Z] [A-Z] [A-Z]$', guess):
+    if re.match(r'^[A-Z] [A-Z] [A-Z] [A-Z]$', guess):  
+            
+        guess_list = [guess[i] for i in range(0, 7, 2) if guess[i] in COLORS]
         
-        guess_list = [guess[i] for i in range(0, 7, 2)]
-        
-        print(guess_list)
-        return True
+        if len(guess_list) == 4:
+            return guess_list
+        else:
+            return None
     else:
-        return False
+        return None
     
 if __name__ == '__main__':
     main()
