5de8d61daafe7f47d1052b67d8bd4ee2d97df37b
Santhosh <santhoshkalish333@gmail.com>
2023-02-10 02:34:09 +0530

    some changes

--- mastermind.py
+++ mastermind.py
@@ -4,35 +4,34 @@ import random
 import re
 
 COLORS = ['R', 'G', 'B', 'Y', 'W', 'O']
-max_tries = 10
+LENTH_CODE = 4
+MAX_TRIES = 10
 
 def main():
     
-    print("""
+    print(f"""
         WELCOME TO MASTERMIND  
         
-        Attempt to guess the 4 digit code...
+        Attempt to guess the {LENTH_CODE} digit code...
         you have 10 tries.
         """)
-    print(f'The colors that could make up the code are: R G B Y W O\n')
+    print('The colors that could make up the code are:', *COLORS)
 
-    # Create an code of 4 random values from colors 
-    code = [random.choice(COLORS) for _ in range(4)]
+    code = generate_code()
     tries = 0
 
-    while tries < max_tries:
+    while tries < MAX_TRIES:
         crct_guess = 0
         incrct_guess = 0
         
         # increase the no of tries for each loop
         tries += 1
+
+        guess = get_user_guess()
         
-        user_guess = input('Guess (space seperated): ')
-        guess_list = validate_guess(user_guess)
-        
-        if guess_list:
-            for i in range(4):
-                if guess_list[i] == code[i]:
+        if guess:
+            for i in range(LENTH_CODE):
+                if guess[i] == code[i]:
                     crct_guess += 1
                 else:
                     incrct_guess += 1
@@ -40,25 +39,31 @@ def main():
             print('Invalid Guess!')
             continue
         
-        if crct_guess == 4:
+        if crct_guess == LENTH_CODE:
             print(f'You guessed the code in {tries} tries!')
             break
         else:
             print(f'Correct position: {crct_guess} | Incorrect possion: {incrct_guess}\n')
             
     # If user run out of tries - print code
-    if tries == max_tries:
-        print(f'You runout of tries :( \nThe code is {code}')          
+    if tries == MAX_TRIES:
+        print('You runout of tries :( \nThe code is', *code)          
 
+
+def generate_code():
+    # Create an code of {LENTH_CODE} random values from colors 
+    code = [random.choice(COLORS) for _ in range(LENTH_CODE)]
+    return code
             
-def validate_guess(guess):
-    # Validate user input and return a list if valid, none if invalid
-    guess = guess.strip().upper()
+            
+def get_user_guess():
+     # Get user guess and Validate it, return a list if valid, None if invalid
+    guess = input('Guess (space seperated): ').strip().upper()
     
     if re.match(r'^[A-Z] [A-Z] [A-Z] [A-Z]$', guess):  
-        # Check - All the user inpu charachters are valid colors    
+        # Check - All the user input charachters are valid colors    
         guess_list = [guess[i] for i in range(0, 7, 2) if guess[i] in COLORS]
-        if len(guess_list) == 4:
+        if len(guess_list) == LENTH_CODE:
             return guess_list
 
     return None
